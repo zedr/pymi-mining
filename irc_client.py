b90fe182623182dadf4ee3bf0ade8833a3e47675
Rigel Di Scala <zedr@zedr.com>
2024-09-08 20:42:25 +0200

    Implement client connect/disconnect

--- irc_client.py
+++ irc_client.py
@@ -1,2 +1,30 @@
+import asyncio
+
+
 class IrcClient:
-    pass
+
+    async def connect(
+            self,
+            server_host: str = "127.1",
+            server_port: int = 6667
+    ):
+        """Connect to an IRC server"""
+        self.reader, self.writer = await asyncio.open_connection(
+            server_host,
+            server_port
+        )
+
+    async def disconnect(self):
+        """Disconnect from the IRC server"""
+        self.writer.close()
+        await self.writer.wait_closed()
+
+
+async def main():
+    client = IrcClient()
+    await client.connect()
+    await client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- irc_client_tests.py
+++ irc_client_tests.py
@@ -1,12 +1,43 @@
+import asyncio
 import unittest
+from typing import Final
 
 from irc_client import IrcClient
 
 
+async def handle_request(reader, writer):
+    data = await reader.readline()
+    print(data)
+
+
 class IrcClientTests(unittest.IsolatedAsyncioTestCase):
     """Async irc client tests"""
+    test_server_host: Final[str] = "127.1"
+    test_server_port = 37337
+
+    async def create_server(self):
+        self._server = await asyncio.start_server(
+            handle_request,
+            "127.1",
+            self.test_server_port,
+            reuse_port=True,
+            reuse_address=True
+        )
+        await self._server.start_serving()
+
+    async def asyncSetUp(self):
+        self._server_task = asyncio.create_task(self.create_server())
+        await self._server_task
+
+    async def asyncTearDown(self):
+        self._server_task.cancel()
 
     async def test_irc_server_client_connection(self):
         """The client can connect to the server"""
         client = IrcClient()
+        await client.connect(self.test_server_host, self.test_server_port)
+        await client.disconnect()
+        self.fail("Finish me!")
+
+    async def test_bla(self):
         self.fail("Finish me!")
