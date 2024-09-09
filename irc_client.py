c1bc9a88cb6b4d94a756500c10a95f290423e5bf
Rigel Di Scala <zedr@zedr.com>
2024-09-09 08:45:41 +0200

    Implement nick, user and channel joining

--- irc_client.py
+++ irc_client.py
@@ -2,19 +2,35 @@ import asyncio
 
 
 class IrcClient:
+    """A simple asynchronous IRC client"""
 
     async def connect(
-            self,
-            server_host: str = "127.1",
-            server_port: int = 6667
-    ):
+        self, server_host: str = "127.1", server_port: int = 6667
+    ) -> None:
         """Connect to an IRC server"""
         self.reader, self.writer = await asyncio.open_connection(
-            server_host,
-            server_port
+            server_host, server_port
         )
 
-    async def disconnect(self):
+    async def send(self, message: str) -> None:
+        """Send a message to the server"""
+        self.writer.write(message.encode() + b"\r\n")
+        await self.writer.drain()
+
+    async def handle(self) -> None:
+        """Handle an incoming message from the server"""
+        while True:
+            line = await self.reader.readline()
+            if line:
+                message = line.decode().strip()
+                source, cmd, *words = message.split(" ")
+                print(source, cmd, *words)
+
+    async def set_nick(self, nick: str) -> None:
+        """Set the nick of the client"""
+        await self.send(f"NICK {nick}")
+
+    async def disconnect(self) -> None:
         """Disconnect from the IRC server"""
         self.writer.close()
         await self.writer.wait_closed()
@@ -23,8 +39,17 @@ class IrcClient:
 async def main():
     client = IrcClient()
     await client.connect()
-    await client.disconnect()
+    await client.set_nick("zedr2")
+    await client.send("USER foo 0 * :foo")
+    await client.send("JOIN #ngi")
+    try:
+        await client.handle()
+    finally:
+        await client.disconnect()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
--- irc_client_tests.py
+++ irc_client_tests.py
@@ -5,23 +5,31 @@ from typing import Final
 from irc_client import IrcClient
 
 
-async def handle_request(reader, writer):
-    data = await reader.readline()
-    print(data)
+class FakeIrcServer:
+    def __init__(self):
+        self.messages = []
+        self.received = asyncio.Event()
+
+    async def handle_request(self, reader, writer):
+        data = await reader.readline()
+        self.messages.append(data)
+        self.received.set()
 
 
 class IrcClientTests(unittest.IsolatedAsyncioTestCase):
     """Async irc client tests"""
+
     test_server_host: Final[str] = "127.1"
     test_server_port = 37337
 
     async def create_server(self):
+        self._irc_server = FakeIrcServer()
         self._server = await asyncio.start_server(
-            handle_request,
+            self._irc_server.handle_request,
             "127.1",
             self.test_server_port,
             reuse_port=True,
-            reuse_address=True
+            reuse_address=True,
         )
         await self._server.start_serving()
 
@@ -31,13 +39,30 @@ class IrcClientTests(unittest.IsolatedAsyncioTestCase):
 
     async def asyncTearDown(self):
         self._server_task.cancel()
+        self._server.close()
 
-    async def test_irc_server_client_connection(self):
+    async def test_client_connection(self):
         """The client can connect to the server"""
         client = IrcClient()
         await client.connect(self.test_server_host, self.test_server_port)
         await client.disconnect()
-        self.fail("Finish me!")
 
-    async def test_bla(self):
-        self.fail("Finish me!")
+    async def test_client_sending_message(self):
+        client = IrcClient()
+        await client.connect(self.test_server_host, self.test_server_port)
+        await client.send("PING")
+        await self._irc_server.received.wait()
+        self.assertEqual([b"PING\r\n"], self._irc_server.messages)
+        await client.disconnect()
+
+    async def test_client_set_nick(self):
+        client = IrcClient()
+        await client.connect(self.test_server_host, self.test_server_port)
+        await client.set_nick("zedr")
+        await self._irc_server.received.wait()
+        self.assertEqual([b"NICK zedr\r\n"], self._irc_server.messages)
+        await client.disconnect()
+
+
+if __name__ == "__main__":
+    unittest.main()
